2630afcf70de1a3768623f07b37527bfc134d1c2
hyasiel <hyasiel1@gmail.com>
2026-02-28 10:05:15 -0500

    main section

--- front/src/features/Main/MainWeb.tsx
+++ front/src/features/Main/MainWeb.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Header } from "@/shared/layout/header/Header";
+import "./mainweb.css"
 //import {Footer}from "@shared/layout/Footer"
 
 //first page route " / "
@@ -9,7 +10,11 @@ export function MainWeb() {
     <Fragment>
       <Header />
       <main>
-        
+        <section className="hero-section">
+          <h1>TU ALIADO PARA LA GESTION DE RECURSOS</h1>
+          <h3>EL ORDEN ES FUNDAMENTAL PARA EL CRECIMIENTO DE TU NEGOCIO</h3>
+          <button>COMENZAR</button>
+        </section>
       </main>
       <footer></footer>
     </Fragment>
